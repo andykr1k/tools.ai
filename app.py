d82cd5ba56ebcf5319e2ae9ebd7fb197d37b5fc6
andykr1k <69021102+andykr1k@users.noreply.github.com>
2023-06-19 10:08:19 -0700

    Adding New Routes

--- app.py
+++ app.py
@@ -1,13 +1,27 @@
 from flask import Flask
 from flask import render_template
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
-
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 @app.route('/')
+@cross_origin()
 def home():
-    return render_template('index.html')
+    return 'hello world'
 
-@app.route('/tools')
-def tools():
-    return render_template('tools.html')
+@app.route('/chat')
+@cross_origin()
+def chat():
+    return 'chat'
+
+@app.route('/text')
+@cross_origin()
+def text():
+    return 'text'
+
+@app.route('/video')
+@cross_origin()
+def video():
+    return 'video'
